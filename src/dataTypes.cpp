d50cf220799271022efdb53d78d2624e23de930b
robledodev <robledo.dev@gmail.com>
2023-01-15 17:47:27 +0100

    first commit

---
+++ src/dataTypes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    // Data Types
+
+    // Integer
+    int apples = 5;
+    int parking = -1;
+
+    // Float - Double
+    float positiveTransaction = 9.99;
+    float negativeTransaction = -23.08;
+    double price = 4.32;
+    double centimeters = -0.67;
+
+    // Boolean
+    bool isGreen = true; //1
+    bool isRed = false; //0
+
+    // Character - String
+    char letter = 'R';
+    string nombre = "Robledo";
+
+    return 0;
+}
