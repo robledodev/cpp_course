6678ee913faeb72c5f34d85fc76a76ca9b3c13ca
robledodev <robledo.dev@gmail.com>
2023-01-18 19:39:12 +0100

    added conditional structure

---
+++ src/conditionalStructure.cpp
@@ -0,0 +1,34 @@
+/*#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string movie = "12";
+
+    // Salida por consola
+    cout << "Pelicula favorita: ";
+
+    // Entrada por consola
+    cin >> movie;
+
+    if(movie == "StarWars") {
+        cout << "La pelicula " << movie << " es buena" << endl;
+    } else if(movie == "Avatar") {
+        cout << "La pelicula " << movie << " no es buena" << endl;
+    } else {
+        cout << "La pelicula " << movie << " no la conozco" << endl;
+    }
+
+    float price;
+
+    cout << "Precio de la prenda: ";
+    cin >> price;
+
+    cout << "El precio " << price << " prenda es ";
+    if(price > 10) {
+        cout << "cara" << endl;
+    } else {
+        cout << "barata" << endl;
+    }
+}*/
